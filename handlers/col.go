6378dc4ccc76e80c3674306378a12fd8795ad47c
Tobias Stevenson <codenamekt@gmail.com>
2015-07-05 23:45:46 -0500

    Adding delete functions for db and col. No routes to them yet as I'm unsure if I want to expose those functions to the world.

--- handlers/col.go
+++ handlers/col.go
@@ -21,3 +21,19 @@ func ColIndex(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, 200, fmt.Sprintf("[%s]", strings.Join(names, ",")))
 	return
 }
+
+func ColDelete(w http.ResponseWriter, r *http.Request) {
+	s := session.New()
+	defer s.Close()
+	vars := mux.Vars(r)
+
+	err := s.DB(vars["db"]).C(vars["collection"]).DropCollection()
+	if err != nil {
+		writeError(w, 500, "Error dropping collection")
+		return
+	}
+
+	w.Header().Add("Content-Length", "0")
+	w.WriteHeader(204)
+	return
+}
--- handlers/db.go
+++ handlers/db.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"strings"
 )
@@ -17,5 +18,21 @@ func DBIndex(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, 200, fmt.Sprintf("[%s]", strings.Join(names, ",")))
+	return
+}
+
+func DBDelete(w http.ResponseWriter, r *http.Request) {
+	s := session.New()
+	defer s.Close()
+	vars := mux.Vars(r)
+
+	err := s.DB(vars["db"]).DropDatabase()
+	if err != nil {
+		writeError(w, 500, "Error dropping database")
+		return
+	}
+
+	w.Header().Add("Content-Length", "0")
+	w.WriteHeader(204)
 	return
 }
